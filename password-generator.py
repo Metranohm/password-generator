c461433bc91556e02fdb92fc439f96c56fbf4cbd
Metranohm <andrewwink@gmail.com>
2023-02-14 12:10:54 -0600

    display chars

--- password-generator.py
+++ password-generator.py
@@ -2,6 +2,10 @@ import random
 import string
 
 def generate_password(min_length, nembers=True, special_characters=True):
-    pass
-  
+    letters = string.ascii_letters
+    digits = string.digits
+    special = string.punctuation
+
+    print(letters, digits, special)
+    
 generate_password(10)
